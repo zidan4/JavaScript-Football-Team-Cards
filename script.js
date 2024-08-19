6912547bac009bafea26404326fc34f05020d11a
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-19 22:13:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
-
 const myFavoriteFootballTeam = {
   team: "Argentina",
   sport: "Football",
